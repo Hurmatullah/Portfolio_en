fa9f8741cd447fb869288c578e2a40aba682c611
= <hrasaa1@gmail.com>
2023-02-11 00:41:03 +0300

    Introduction tab added

---
+++ src/pages/introduction.jsx
@@ -0,0 +1,31 @@
+const Introduction = () => {
+    return (
+      // <!-- Introduction -->
+      <div class="intro section" id="about">
+        <div class="container">
+          <p>Привет, Я Хурматулла Карими</p>
+          <div class="units-row units-split wrap">
+            <div class="unit-20">
+              <img src="img/hurmat.jpeg" alt="Avatar" />
+            </div>
+            <div class="unit-80">
+              <h1>
+                Я живу в
+                <br />
+                <span id="typed"></span>
+              </h1>
+            </div>
+            <p>
+              Высокоорганизованный разработчик программного обеспечения, имеющий
+              проверенный опыт использования передовой инженерной практики для
+              создания программного обеспечения для новых, инновационных и
+              прибыльных продуктов и решений.
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  };
+  
+  export default Introduction;
+  
