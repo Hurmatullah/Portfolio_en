e6da9a537c29eb0ad71e5b2c425624d98e4b1d2f
= <hrasaa1@gmail.com>
2023-02-11 00:40:15 +0300

    Experience tab added

---
+++ src/pages/experience.jsx
@@ -0,0 +1,40 @@
+const Experience = () => {
+    return (
+      // <!-- Work Experience -->
+      <div class="work section second" id="experiences">
+        <div class="container">
+          <h1>
+            работа
+            <br />
+            опыт
+          </h1>
+          <ul class="work-list">
+            <li>Февраль 2020 - сентябрь 2021</li>
+            <li>
+              <a href="https://iap.af">
+                Intellectual Application and Product (IAP)
+              </a>
+            </li>
+            <li>Web-разработчик И помощник по веб-хостингу</li>
+          </ul>
+          <ul class="work-list">
+            <li>Июль 2019 - декабрь 2019</li>
+            <li>
+              <a href="#">Avatech</a>
+            </li>
+            <li>Web-разработчик</li>
+          </ul>
+          <ul class="work-list">
+            <li>Май 2018 - сентябрь 2018</li>
+            <li>
+              <a href="https://cyberaan.com/">Cyberaan</a>
+            </li>
+            <li>Разработчик Laravel (стажер)</li>
+          </ul>
+        </div>
+      </div>
+    );
+  };
+  
+  export default Experience;
+  
