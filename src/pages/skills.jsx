7dd69ca28caceadb3e49c724fb9034bd298729f9
= <hrasaa1@gmail.com>
2023-02-11 00:42:38 +0300

    Skills tab added

---
+++ src/pages/skills.jsx
@@ -0,0 +1,28 @@
+const Skills = () => {
+    return (
+      // <!-- Technical Skills -->
+      <div class="skills section second" id="skills">
+        <div class="container">
+          <h1>
+            технический
+            <br />
+            навыки
+          </h1>
+          <ul class="skill-list list-flat">
+            <li>Веб-технологии</li>
+            <li>
+              HTML / CSS / Laravel / PHP / Javascript / Wordpress / Rest Api /
+              Reactjs / Bootstrap
+            </li>
+          </ul>
+          <ul class="skill-list list-flat">
+            <li>База данных</li>
+            <li>MySQL </li>
+          </ul>
+        </div>
+      </div>
+    );
+  };
+  
+  export default Skills;
+  
