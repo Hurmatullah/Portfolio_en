75ff0275698d856413b422b75374427c6f7a1ede
= <hrasaa1@gmail.com>
2023-02-16 21:55:15 +0300

    Contact page bug fixed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import "./App.css";
+import React, { useEffect } from "react";
+import AOS from "aos";
+// import "aos/dist/aos.css";
+import "../../public/css/vendor/aos.css";
 // import Footer from "./includes/footer";
 // import Header from "./includes/header";
 import About from "./pages/about";
@@ -8,6 +12,10 @@ import Portfolio from "./pages/portfolio";
 import Blogs from "./pages/blogs";
 
 function App() {
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  }, []);
   return (
     <div>
       {/* <Header></Header>
